15bb541ff428f3730df7a42f78ff941b6ffe9427
Washington Obungu <tobungu@gmail.com>
2022-07-06 19:34:24 +0000

    Initial task 1 upload

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ Defines a class Cache """
-from typing import Union
+from typing import Union, Callable
 import redis
 import uuid
 
@@ -9,8 +9,8 @@ class Cache:
     """ Creates a redis cache """
     def __init__(self):
         """ Initializes a redis instance """
-        self._redis = redis.Redis()
-        self._redis.flushdb()
+        self.__redis = redis.Redis()
+        self.__redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ Generates a random uuid key and stores 'data' in Redis
@@ -20,5 +20,26 @@ class Cache:
             The generated uuid string
         """
         key = str(uuid.uuid4())
-        self._redis.set(key, data)
+        self.__redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[
+            str, bytes, int, float]:
+        """ Returns the value associated with 'key' in its original data-type
+        """
+        if self.__redis.exists(key):
+            if fn is not None:
+                return fn(self.__redis.get(key))
+            else:
+                return self.__redis.get(key)
+
+    def get_str(self, key: str) -> str:
+        """ Converts a byte data type returned by the 'get' method
+            to a string
+        """
+        return self.get(key, lambda val: val.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """ Converts a byte data type returned by the get method to an int
+        """
+        return self.get(key, lambda val: int(val))
